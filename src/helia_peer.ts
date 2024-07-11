8ef7e4c1846d367777ecaa1b7f7cca252ec390a8
unknown <dolphin2410@outlook.kr>
2024-07-11 16:20:34 +0900

    improve ipfs

---
+++ src/helia_peer.ts
@@ -0,0 +1,3 @@
+import { createNode } from "./network/ipfs";
+
+createNode()
--- src/network/ipfs.ts
+++ src/network/ipfs.ts
@@ -1,32 +1,42 @@
 import { CID } from "multiformats/cid"
 import fs from "fs"
 import { FsBlockstore } from "blockstore-fs"
+import { FsDatastore } from "datastore-fs"
 
-const createNode = async () => {
+export const createNode = async () => {
     const { createHelia } = await import("helia")
-    const { unixfs } = await import("@helia/unixfs")
 
     const blockstore = new FsBlockstore("./ipfs-storage/")
+    const datastore = new FsDatastore("./ipfs-datastore")
 
     const helia = await createHelia({
-        blockstore
+        blockstore,
+        datastore
     })
     
-    const helia_fs = unixfs(helia)
+    console.log(`ID: ${helia.libp2p.peerId}`)
+    console.log("MULTIADDRs")
 
-    return helia_fs
+    console.log(helia.libp2p.getMultiaddrs())
+
+    return helia
 }
 
 export async function download_from_ipfs(hash: string, target: string) {
-    const heliaNode = await createNode()
+    const { unixfs } = await import("@helia/unixfs")
+
+    const helia = await createNode()
+    const helia_fs = unixfs(helia)
 
     const stream = fs.createWriteStream(target)
 
     stream.once("open", async (_fd) => {
         const cid = CID.parse(hash)
-        for await (const buf of heliaNode.cat(cid)) {
+        for await (const buf of helia_fs.cat(cid)) {
             stream.write(buf)
         }
         stream.end()
     })
+
+    helia.stop()
 }
